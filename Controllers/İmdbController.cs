41311351067857154ea5e859594b994cc6cad716
YUSUF KAÇMAZ <yusufkacmzz@gmail.com>
2025-02-27 21:18:43 +0300

    Add project files.

---
+++ Controllers/İmdbController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApi.WebUI.Models;
+
+namespace RapidApi.WebUI.Controllers
+{
+    public class İmdbController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+          
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb236.p.rapidapi.com/imdb/top250-movies"),
+                Headers =
+    {
+        { "x-rapidapi-key", "61612a3788msh4b0bdf93637d781p1ab924jsn60c8aa167719" },
+        { "x-rapidapi-host", "imdb236.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var jsonbody = await response.Content.ReadAsStringAsync();
+
+                var values = JsonConvert.DeserializeObject<List<İmdbViewModel.Movie>>(jsonbody);
+
+                var dramList = values.Where(x=> x.genres.Contains("Drama")).ToList();   
+                return View(dramList);
+            }
+           
+        }
+    }
+}
---
+++ Models/İmdbViewModel.cs
@@ -0,0 +1,46 @@
+﻿namespace RapidApi.WebUI.Models
+{
+    public class İmdbViewModel
+    {
+
+        
+        
+         public List<Movie> Movies { get; set; }
+        
+
+        public class Movie
+        {
+            public string id { get; set; }
+            public string url { get; set; }
+            public string primaryTitle { get; set; }
+            public string originalTitle { get; set; }
+            public string type { get; set; }
+            public string description { get; set; }
+            public string primaryImage { get; set; }
+            public string contentRating { get; set; }
+            public int startYear { get; set; }
+            public object endYear { get; set; }
+            public string releaseDate { get; set; }
+            public string[] interests { get; set; }
+            public string[] countriesOfOrigin { get; set; }
+            public string[] externalLinks { get; set; }
+            public string[] spokenLanguages { get; set; }
+            public string[] filmingLocations { get; set; }
+            public Productioncompany[] productionCompanies { get; set; }
+            public long? budget { get; set; }
+            public long? grossWorldwide { get; set; }
+            public List<string> genres { get; set; }
+            public bool isAdult { get; set; }
+            public int runtimeMinutes { get; set; }
+            public float averageRating { get; set; }
+            public int numVotes { get; set; }
+        }
+
+        public class Productioncompany
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+        }
+
+    }
+}
